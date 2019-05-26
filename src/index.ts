3c69a9250e90006f717c06dce7c04ce7e4c9f5d1
Yasser Nascimento <yassern@protonmail.com>
2019-05-26 18:39:21 -0300

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-// tslint:disable-next-line:no-console
-console.log("hello world");
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => res.send("Hello World"));
+app.listen(3000);
