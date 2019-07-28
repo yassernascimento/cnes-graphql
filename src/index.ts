772d08aff11d6cf40a8fe15c7d5ff2948a5924f7
Yasser Nascimento <yassern@protonmail.com>
2019-07-28 19:36:08 -0300

    inject database on server.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import { Database } from "./database";
 import { Server } from "./server";
 
 (async function start() {
-  const server = new Server();
-  await server.start();
-
   const database = new Database();
   await database.connect();
-  await database.disconnect();
+
+  const server = new Server(database);
+  await server.start();
 })();
