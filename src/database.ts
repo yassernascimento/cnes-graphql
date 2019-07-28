3e7dc57e174913dda8892b2a4170f9e41dd95ff3
Yasser Nascimento <yassern@protonmail.com>
2019-07-28 19:41:02 -0300

    query database for estabelecimento.

--- src/database.ts
+++ src/database.ts
@@ -5,14 +5,22 @@ export class Database {
   private client = new MongoClient(DATABASE_URL, { useNewUrlParser: true });
 
   public async connect() {
-    await this.client.connect();
-    const connection = this.client.isConnected() ? "Connected" : "Disconnected";
-    console.log(`Database is ${connection}`);
+    if (!this.client.isConnected()) {
+      await this.client.connect();
+    }
   }
 
-  public async disconnect() {
-    if (this.client.isConnected()) {
-      await this.client.close();
+  public async getEstabelecimento(coUnidade: number) {
+    this.connectedGuard();
+    return await this.client
+      .db("paciente")
+      .collection("estabelecimentos")
+      .findOne({ CO_UNIDADE: Number(coUnidade) });
+  }
+
+  private connectedGuard() {
+    if (!this.client.isConnected()) {
+      throw new Error("Database not connected");
     }
   }
 }
